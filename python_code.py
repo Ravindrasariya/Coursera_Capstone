6175a5eb3afc2da09feee4863bfa71e2d0f83897
Ravindra Sariya <sariya@Ravindras-MacBook-Air.local>
2020-05-24 23:29:04 +0530

    Week1 peer-graded Assignment

--- python_code.py
+++ python_code.py
@@ -1,1 +1,8 @@
-print("Hello Github!")
+# This notebook will be used for the  Applied Data Science Capstone Project.
+
+import pandas as pd
+import numpy as np
+
+print("Hello Capstone Project Course!")
+
+
